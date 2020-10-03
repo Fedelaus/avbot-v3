967b7dc9a29e877121b4bf6b12b69074c785b25e
Restyled.io <commits@restyled.io>
2020-10-03 07:02:56 +0000

    Restyled by clang-format

--- src/app/commands/misc/zulu.js
+++ src/app/commands/misc/zulu.js
@@ -1,34 +1,38 @@
-const { MessageEmbed } = require('discord.js');
-const { Command } = require('discord.js-commando');
+const {MessageEmbed} = require('discord.js');
+const {Command} = require('discord.js-commando');
 const dayjs = require('dayjs');
 const Avwx = require('../../utils/Avwx');
 
 module.exports = class ZuluCommand extends Command {
   constructor(client) {
     super(client, {
-      name: 'zulu',
-      group: 'misc',
-      memberName: 'zulu',
-      description: 'Gives you the current Zulu time.',
-      args: [
+      name : 'zulu',
+      group : 'misc',
+      memberName : 'zulu',
+      description : 'Gives you the current Zulu time.',
+      args : [
         {
-          key: 'icao',
-          type: 'string',
-          prompt: 'Enter ICAO code',
-          default: '',
-          parse: (val, msg, arg) => val.toUpperCase(),
+          key : 'icao',
+          type : 'string',
+          prompt : 'Enter ICAO code',
+          default : '',
+          parse : (val, msg, arg) => val.toUpperCase(),
         },
         {
-          key: 'localtime',
-          type: 'string',
-          prompt: 'Enter Local time',
-          default: '',
-          validate: (val, msg, arg) => {
-            if (!val) return true;
-            if (val.length !== 4) return 'Local time must be in HHMM format';
-            const [HH, MM] = [val.substr(0, 2), val.substr(2)];
-            if (23 < HH || HH < 0) return 'Invalid HH';
-            if (59 < MM || MM < 0) return 'Invalid MM';
+          key : 'localtime',
+          type : 'string',
+          prompt : 'Enter Local time',
+          default : '',
+          validate : (val, msg, arg) => {
+            if (!val)
+              return true;
+            if (val.length !== 4)
+              return 'Local time must be in HHMM format';
+            const [HH, MM] = [ val.substr(0, 2), val.substr(2) ];
+            if (23 < HH || HH < 0)
+              return 'Invalid HH';
+            if (59 < MM || MM < 0)
+              return 'Invalid MM';
             return true;
           },
         },
@@ -36,13 +40,13 @@ module.exports = class ZuluCommand extends Command {
     });
   }
 
-  async run(message, { icao, localtime }) {
+  async run(message, {icao, localtime}) {
     if (!(icao || localtime)) {
       const timestring = dayjs.utc().format('DD/MM HH:mm');
       const zuluEmbed = new MessageEmbed()
-        .setTitle('ZULU Time')
-        .setColor('#1a8fe3')
-        .setDescription(`${timestring} Z`);
+                            .setTitle('ZULU Time')
+                            .setColor('#1a8fe3')
+                            .setDescription(`${timestring} Z`);
 
       return message.embed(zuluEmbed);
     }
--- src/app/commands/weather/atis.js
+++ src/app/commands/weather/atis.js
@@ -1,33 +1,33 @@
 const Discord = require('discord.js');
-const { Command } = require('discord.js-commando');
+const {Command} = require('discord.js-commando');
 const Avwx = require('../../utils/Avwx');
 
 module.exports = class AtisCommand extends Command {
   constructor(client) {
     super(client, {
-      name: 'atis',
-      group: 'weather',
-      memberName: 'atis',
-      aliases: [],
-      description: 'Gives you live ATIS of the chosen airport.',
-      examples: ['atis <icao>'],
-      args: [
+      name : 'atis',
+      group : 'weather',
+      memberName : 'atis',
+      aliases : [],
+      description : 'Gives you live ATIS of the chosen airport.',
+      examples : [ 'atis <icao>' ],
+      args : [
         {
-          key: 'icao',
-          prompt: 'What ICAO would you like the bot to give ATIS for?',
-          type: 'string',
+          key : 'icao',
+          prompt : 'What ICAO would you like the bot to give ATIS for?',
+          type : 'string',
         },
       ],
     });
   }
 
-  async run(msg, { icao }) {
+  async run(msg, {icao}) {
     const atisEmbed = new Discord.MessageEmbed()
-      .setTitle(`ATIS for ${icao.toUpperCase()}`)
-      .setTimestamp();
+                          .setTitle(`ATIS for ${icao.toUpperCase()}`)
+                          .setTimestamp();
 
     try {
-      const { speech } = await Avwx.getMetar(icao);
+      const {speech} = await Avwx.getMetar(icao);
 
       atisEmbed.setColor('#0099ff').setDescription(speech);
     } catch (error) {
